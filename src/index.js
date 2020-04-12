71f12e8bdb26c2addf50349b12f3a68b246a8e48
feuler <feuler@feulers-iMac-i5-2.local>
2020-04-12 12:39:52 -0400

    adding redirect code for multi- URLs to aws-exports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
-import Amplify from 'aws-amplify'
+import Amplify, { Auth } from 'aws-amplify'
 import config from './aws-exports'
-Amplify.configure(config)
+
+var urlsIn = config.oauth.redirectSignIn.split(",");
+var urlsOut = config.oauth.redirectSignOut.split(",");
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType
+};
+var hasLocalhost  = (hostname) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
+var hasHostname   = (hostname) => Boolean(hostname.includes(window.location.hostname));
+var isLocalhost   = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach((e) =>   { if (hasLocalhost(e)) { oauth.redirectSignIn = e; }});
+  urlsOut.forEach((e) =>  { if (hasLocalhost(e)) { oauth.redirectSignOut = e; }});
+}
+else {
+  urlsIn.forEach((e) =>   { if (hasHostname(e)) { oauth.redirectSignIn = e; }});
+  urlsOut.forEach((e) =>  { if (hasHostname(e)) { oauth.redirectSignOut = e; }});
+}
+var configUpdate = config;
+configUpdate.oauth = oauth;
+Amplify.configure(configUpdate);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 
 ReactDOM.render(<App />, document.getElementById('root'));;
