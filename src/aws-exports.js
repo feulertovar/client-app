547de3da776a2be664fd1a7bd6bbcfe3a343c401
feuler <feuler@feulers-iMac-i5-2.local>
2020-04-12 01:26:21 -0400

    adding localhost redirect  URLs to aws-exports

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -15,8 +15,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "https://master.dmish4eskhxxv.amplifyapp.com/",
-        "redirectSignOut": "https://master.dmish4eskhxxv.amplifyapp.com/",
+        "redirectSignIn": "http://localhost:3000/,https://master.dmish4eskhxxv.amplifyapp.com/",
+        "redirectSignOut": "http://localhost:3000/,https://master.dmish4eskhxxv.amplifyapp.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS"
